c642dae324a52779575ab7c5625f598f3e1ab139
abhiramips28 <abhiramips1000@gmail.com>
2023-07-16 15:37:40 +0530

    first commit

---
+++ todoapp/todo/urls.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from .views import TaskList, TaskCreate, TaskDelete, TaskDetailView, TaskUpdate, CustomLoginView, RegisterView, HomeView
+
+urlpatterns = [
+    path('',HomeView.as_view(),name='home'),
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    path('register/',RegisterView.as_view(),name='register'),
+    path('list/', TaskList.as_view(), name='task'),
+    path('create/',TaskCreate.as_view(),name='task-create'),
+    path('update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('delete/<int:pk>/',TaskDelete.as_view(),name='task-delete'),
+    path('details/<int:pk>/', TaskDetailView.as_view(), name='task-details')
+]
